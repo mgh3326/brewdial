70cd91d48fc4cd41806696bb592d571a4cbc9fbd
robin <robin@watcha.com>
2026-08-26 15:23:46 +0900

    ROB-1316: scaffold Kotlin Spring Boot API module

---
+++ apps/api-kt/src/main/kotlin/com/brewdial/api/BrewdialApiKtApplication.kt
@@ -0,0 +1,11 @@
+package com.brewdial.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrewdialApiKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<BrewdialApiKtApplication>(*args)
+}
